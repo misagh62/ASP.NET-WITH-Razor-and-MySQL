ad7a2c77e1afd5d3e95e11741bd8ed96f0c7983c
Misagh Sh <44715016+misagh62@users.noreply.github.com>
2023-02-11 20:43:57 +0100

    Update ApplicationDbContext.cs

--- Model/ApplicationDbContext.cs
+++ Model/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+//try to test with git
 namespace BookListRazor.Model
 {
     public class ApplicationDbContext : DbContext
